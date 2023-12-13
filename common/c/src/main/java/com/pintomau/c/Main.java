4fe9f62d52bf739040d3870f35377f8abd084587
Mauro Pinto <mauro.s.pinto@gmail.com>
2023-12-13 21:53:27 +0000

    a c change (#4)

--- common/c/src/main/java/com/pintomau/c/Main.java
+++ common/c/src/main/java/com/pintomau/c/Main.java
@@ -3,5 +3,6 @@ package com.pintomau.c;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello c.Main!");
+        System.out.println("Hello c.Main!");
     }
 }
