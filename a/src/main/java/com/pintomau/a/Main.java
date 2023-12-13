d95d835f348072dfd706b02e771fee10eadc3dd4
Mauro Pinto <mauro.s.pinto@gmail.com>
2023-12-13 17:46:09 +0000

    a change in a wololo

--- a/src/main/java/com/pintomau/a/Main.java
+++ a/src/main/java/com/pintomau/a/Main.java
@@ -3,5 +3,6 @@ package com.pintomau.a;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello a.Main!");
+        System.out.println("Hello a.Main!");
     }
 }
