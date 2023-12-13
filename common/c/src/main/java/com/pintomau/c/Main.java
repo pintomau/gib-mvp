45b28d6f9c76470541208c6ee545707b7cd6f333
Mauro Pinto <mauro.s.pinto@gmail.com>
2023-12-13 15:54:21 +0000

    a change in c and d

--- common/c/src/main/java/com/pintomau/c/Main.java
+++ common/c/src/main/java/com/pintomau/c/Main.java
@@ -3,8 +3,5 @@ package com.pintomau.c;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello c.Main!");
-        System.out.println("Hello c.Main!");
-        System.out.println("Hello c.Main!");
-        System.out.println("Hello c.Main!");
     }
 }
