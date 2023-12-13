6f040ddfacc772ab234c0ac852264e5fd2e8b4db
Mauro Pinto <mauro.s.pinto@gmail.com>
2023-12-13 21:41:03 +0000

    a change

--- a/src/main/java/com/pintomau/a/Main.java
+++ a/src/main/java/com/pintomau/a/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello a.Main!");
         System.out.println("something else");
+        System.out.println("something else");
     }
 }
