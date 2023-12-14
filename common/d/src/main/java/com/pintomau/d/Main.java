dfc3e52bb83fc005dc50633da2da12b34ce9228d
Mauro Pinto <mauro.s.pinto@gmail.com>
2023-12-14 13:58:26 +0000

    deez feature

--- common/d/src/main/java/com/pintomau/d/Main.java
+++ common/d/src/main/java/com/pintomau/d/Main.java
@@ -3,5 +3,6 @@ package com.pintomau.d;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello d.Main");
+        System.out.println("deez feature");
     }
 }
