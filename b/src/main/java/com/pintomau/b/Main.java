934d13ac14a7fa04d163f179a74e4646f3ae1520
Mauro Pinto <mauro.s.pinto@gmail.com>
2023-12-14 13:56:26 +0000

    some b feature

--- b/src/main/java/com/pintomau/b/Main.java
+++ b/src/main/java/com/pintomau/b/Main.java
@@ -3,5 +3,6 @@ package com.pintomau.b;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello b.Main!");
+        System.out.println("a b feature");
     }
 }
