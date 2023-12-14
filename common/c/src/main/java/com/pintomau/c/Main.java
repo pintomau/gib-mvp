6569a99153069e52fc9056fbf3e0fa0929485efd
Mauro Pinto <mauro.s.pinto@gmail.com>
2023-12-14 13:57:02 +0000

    some c change

--- common/c/src/main/java/com/pintomau/c/Main.java
+++ common/c/src/main/java/com/pintomau/c/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello c.Main!");
         System.out.println("Hello c.Main!");
+        System.out.println("do you c this?");
     }
 }
